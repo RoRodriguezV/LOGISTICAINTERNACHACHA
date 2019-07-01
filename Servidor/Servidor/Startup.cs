c3a5da04f7ceb979793a1d31ee6deb0a336d35cf
eavtono7 <eavtono7@gmail.com>
2019-07-01 00:02:20 -0400

    Lo ultimo

---
+++ Servidor/Servidor/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Servidor.Startup))]
+
+namespace Servidor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
